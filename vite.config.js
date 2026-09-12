086d2122b24e0bd300002f6898f7165db57d6e0a
suhitha1929 <suhitha1929@gmail.com>
2026-09-12 10:10:55 +0530

    Updated calculator styling

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/REACT-CALCULATOR/',
+    base: '/Calculator_react/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
